878fa912cf6ba17f24b7896ec911aae0d50c675a
Xushnudd <boshhacker1@gmail.com>
2025-06-14 13:47:01 +0500

    bajarildi

--- if06.py
+++ if06.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a = n//10000
+    b = (n//1000)%10
+    d = (n//100)%10
+    e = (n//10)%10
+    f = n%10
+    if a>=b and a>=d and a>=e and a>=f:
+        return 5
+    elif b>=a and b>=d and b>=e and b>=f:
+        return 4
+    elif d>=a and d>=b and d>=e and d>=f:
+        return 3
+    elif e>=a and e>=b and e>=d and e>=f:
+        return 2
+    else:
+        return 1
+print(main(12765))
